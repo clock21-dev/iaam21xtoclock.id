9ba153338f1b547f47f389e3a90f0fcb49771dec
clock21 <ilhamardi1230@gmail.com>
2026-05-17 21:58:22 +0700

    Update mlbbconfig.js

--- grubjb/mlbb/mlbbconfig.js
+++ grubjb/mlbb/mlbbconfig.js
@@ -14,22 +14,22 @@ const CONFIG = {
     {
       icon: "fa-gamepad",
       title: "GRUB JUAL BELI MLBB 1",
-      url: "https://wa.me/628xxxxxxxxxx"
+      url: "https://chat.whatsapp.com/Lg081x4yOJZAOqVDVoP3q6"
     },
     {
       icon: "fa-gamepad",
       title: "GRUB JUAL BELI MLBB 2",
-      url: "https://wa.me/628xxxxxxxxxx"
+      url: "https://chat.whatsapp.com/GJNOartWIcw42WCvEoJf2U"
     },
     {
       icon: "fa-gamepad",
       title: "GRUB JUAL BELI MLBB 3", 
-      url: "https://wa.me/628xxxxxxxxxx"
+      url: "https://chat.whatsapp.com/IqGU8wx61OaCPhs1c9FcEH"
     },
     {
       icon: "fa-gamepad",
       title: "GRUB JUAL BELI MLBB 4",
-      url: "https://tiktok.com/@username"
+      url: "https://chat.whatsapp.com/COFoIlX5JcKHRPpbZ2PRHH"
     },
     {
       icon: "fa-home",
